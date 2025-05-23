f425ea8c49ed0f18e5454feb52f244758e8eb3bc
0x676e67 <gngppz@gmail.com>
2025-05-23 18:29:48 +0800

    update MSRV

--- src/frame/mod.rs
+++ src/frame/mod.rs
@@ -59,6 +59,7 @@ pub use self::ping::Ping;
 pub use self::priority::{Priority, StreamDependency};
 pub use self::reason::Reason;
 pub use self::reset::Reset;
+#[allow(unused_imports)]
 pub use self::settings::{Setting, SettingId, SettingOrder, Settings};
 pub use self::stream_id::{StreamId, StreamIdOverflow};
 pub use self::window_update::WindowUpdate;
